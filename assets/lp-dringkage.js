82fcd0ea8d74a9e87739159ba3ba93ffae011c47
hasankarim18 <hallysust@gmail.com>
2025-02-01 21:06:16 +0600

    ui landing page

--- assets/lp-dringkage.js
+++ assets/lp-dringkage.js
@@ -1,34 +1,3 @@
-var swiper = new Swiper('.mySwiper2', {
-  slidesPerView: 2.2,
-  spaceBetween: 30,
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-  },
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-});
-
-var acc = document.getElementsByClassName('accordion');
-var i;
-
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener('click', function () {
-    this.classList.toggle('active');
-    this.parentElement.classList.toggle('active');
-
-    var pannel = this.nextElementSibling;
-
-    if (pannel.style.display === 'block') {
-      pannel.style.display = 'none';
-    } else {
-      pannel.style.display = 'block';
-    }
-  });
-}
-
 /*
 var acc = document.getElementsByClassName('cart_box');
 var i;
